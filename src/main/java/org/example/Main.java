b5a406cc5dade5b10e647b3c1f8a90ccfd1ec16e
okavsar9-boop <okavsar9@gmail.com>
2026-05-30 20:10:33 +0200

    Imported try and catch block to handle the errors that might happen while connecting to the database

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,82 +1,94 @@
 package org.example;
-
-import java.sql.SQLOutput;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         String url = "jdbc:postgresql://localhost:5432/MyFirstApp";
+        String user = "postgres";
+        String password = "ominakavsar11";
 
-        // Let's start with the entry point
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            System.out.println("Success : Secure Connection stablished with PostgreSql");
+            // Let's start with the entry point
+            Scanner scanner = new Scanner(System.in);
+            boolean running = true;
+            int currentSessionUserId = 0;
 
-            System.out.println("Choose One Option :\n");
-            System.out.println("1 : Register User ");
-            System.out.println("2 : Log In  ");
-            System.out.println("3 : Exit ");
+            while (running) {
 
-            // We check the user's intended option here
+                System.out.println("Choose One Option :\n");
+                System.out.println("1 : Register User ");
+                System.out.println("2 : Log In  ");
+                System.out.println("3 : Exit ");
 
-            int option = scanner.nextInt();
-            switch (option) {
-                case 1:
+                // We check the user's intended option here
 
-                    System.out.println("Enter your name :");
-                    String name = scanner.nextLine();
+                int option = scanner.nextInt();
+                switch (option) {
+                    case 1:
 
-                    System.out.println("Enter your surname :");
-                    String surname = scanner.nextLine();
+                        System.out.println("Enter your name :");
+                        String name = scanner.nextLine();
 
-                    System.out.println("Select your card type:");
-                    System.out.println("1: National Card");
-                    System.out.println("2: Visa Card");
+                        System.out.println("Enter your surname :");
+                        String surname = scanner.nextLine();
 
-                    // Here user should enter their card type because each card type has different actions
+                        System.out.println("Select your card type:");
+                        System.out.println("1: National Card");
+                        System.out.println("2: Visa Card");
 
-                    int card = scanner.nextInt();
+                        // Here user should enter their card type because each card type has different actions
 
-                    switch (card) {
-                        case 1:
-                            System.out.println("You chose National card");
-                            break;
-                        case 2:
-                            System.out.println("You chose Visa card");
-                            break;
-                        default:
-                            System.out.println("Invalid selection !");
-                            break;
-                    }
+                        int card = scanner.nextInt();
 
-                    // Here If user chooses the option 2 that means they are already registered
-                    // We have to just operate the another action
+                        switch (card) {
+                            case 1:
+                                System.out.println("You chose National card");
+                                break;
+                            case 2:
+                                System.out.println("You chose Visa card");
+                                break;
+                            default:
+                                System.out.println("Invalid selection !");
+                                break;
+                        }
 
-                case 2:
-                    System.out.println("Enter your User ID number :");
-                    int id_number = scanner.nextInt();
+                        // Here If user chooses the option 2 that means they are already registered
+                        // We have to just operate the another action
 
-                    // Here we are going to check if the ID is valid or not
+                    case 2:
+                        System.out.println("Enter your User ID number :");
+                        int id_number = scanner.nextInt();
+
+                        // Here we are going to check if the ID is valid or not
 
 
-                    System.out.println("How much Money you want to deposit ? :");
-                    double money = scanner.nextDouble();
-                    if (money > 0) {
+                        System.out.println("How much Money you want to deposit ? :");
+                        double money = scanner.nextDouble();
 
-                    }
-                    break;
+                        break;
 
-                // The user chose exit section ,so we terminate all things !
+                    // The user chose exit section ,so we terminate all things !
 
-                case 3:
-                    System.out.println("Exit chosen");
-                    System.exit(0);
-                    break;
+                    case 3:
+                        System.out.println("Exit chosen");
+                        System.exit(0);
+                        break;
 
                     // If user enters the invalid number or other than from , 1 - 3 it will be show an error
-                default:
-                    System.out.println("Invalid choice");
-                    break;
+                    default:
+                        System.out.println("Invalid choice");
+                        break;
+                }
             }
+        }
+        catch (SQLException e ){
+            System.out.println("Error Occured : Something went wrong with the database connection");
+            e.printStackTrace();
         }
     }
 }
