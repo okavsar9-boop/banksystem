ad257fb1e128a0cc9965c8bd8de55c5dad79fbad
okavsar9-boop <okavsar9@gmail.com>
2026-06-08 16:16:35 +0200

    created all the necassary methods inside the Bank class

--- src/main/java/org/example/Bank.java
+++ src/main/java/org/example/Bank.java
@@ -1,4 +1,50 @@
 package org.example;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class Bank {
+    private static final double TRANSACTION_FEE = 2.0;
+
+    public static double getTransactionFee() {
+        return TRANSACTION_FEE;
+    }
+
+    private Connection connection;
+
+    public Bank(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void register(String name, String surname, LocalDate dob, String passportId, String pin, Double balance, String cardType) throws SQLException {
+        User user = new User(name, surname, dob, passportId, pin, balance, cardType);
+        UserRepository.insertingNewIDToDatabase(connection, user);
+    }
+
+    public void deposit(String passportId, Double amount) throws SQLException {
+        if (amount > 0) {
+            TransactionRepository.insertTransaction(connection, passportId, null, "DEPOSIT", amount);
+        } else {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+    }
+
+    public void withdraw(String passportId, Double amount) throws SQLException {
+        double balance = TransactionRepository.getBalance(connection, passportId);
+        if (amount > 0 && amount + TRANSACTION_FEE <= balance) {
+            TransactionRepository.insertTransaction(connection, passportId, null, "WITHDRAW", amount);
+        } else {
+            throw new IllegalArgumentException("Balance cannot be negative ");
+        }
+    }
+
+    public void transfer(String senderPassportId, String receiverPassportId, Double amount) throws SQLException {
+        double balance = TransactionRepository.getBalance(connection, senderPassportId);
+        if (amount > 0 && amount + TRANSACTION_FEE <= balance) {
+            TransactionRepository.insertTransaction(connection, senderPassportId, receiverPassportId, "TRANSFER", amount);
+        }else{
+           throw new IllegalArgumentException("Either amount is invalid or balance is not enough!");
+        }
+    }
 }
+
